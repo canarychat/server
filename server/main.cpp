103fbf6e839f9af2bc9526213ddfd52105bfa030
Lambert-Rao <lambert-teddy-smith@outlook.com>
2023-05-29 13:21:40 +0800

    v0.2.0

--- server/main.cpp
+++ server/main.cpp
@@ -5,103 +5,133 @@
 #include "DataManager.h"
 #include "ChatHttp.h"
 
-#include "support/poco_headers.h"
+#include "Poco/Net/HTTPServer.h"
+#include "Poco/Net/WebSocket.h"
+#include "Poco/Net/ServerSocket.h"
+#include "Poco/Net/HTTPRequestHandlerFactory.h"
+#include "Poco/Net/HTTPRequestHandler.h"
+#include "Poco/Net/HTTPServerRequest.h"
+#include "Poco/Net/HTTPServerResponse.h"
+#include "Poco/Net/WebSocket.h"
+#include "Poco/Net/HTTPServerParams.h"
+#include "Poco/Net/HTTPServerRequestImpl.h"
+#include "Poco/Net/HTTPServerResponseImpl.h"
+#include "Poco/Net/HTTPRequestHandlerFactory.h"
+#include "Poco/Net/HTTPRequestHandler.h"
+#include "Poco/Net/HTTPServerRequest.h"
+#include "Poco/Net/HTTPServerResponse.h"
+#include "Poco/Net/WebSocket.h"
+#include "Poco/Net/HTTPServerParams.h"
+#include "Poco/Util/ServerApplication.h"
+#include "Poco/Util/OptionSet.h"
+#include "Poco/Util/OptionCallback.h"
+#include "Poco/Util/Option.h"
+#include "Poco/Util/HelpFormatter.h"
+#include "Poco/Util/AbstractConfiguration.h"
+#include "Poco/Util/LoggingConfigurator.h"
+#include "Poco/Util/PropertyFileConfiguration.h"
+#include "Poco/Util/Application.h"
+#include "Poco/Net/HTTPRequest.h"
+#include "Poco/Timestamp.h"
+#include "Poco/DateTimeFormatter.h"
+#include "Poco/DateTimeFormat.h"
+#include "Poco/PatternFormatter.h"
+#include "Poco/FormattingChannel.h"
+#include "Poco/ConsoleChannel.h"
+
+using Poco::Net::ServerSocket;
+using Poco::Net::HTTPRequestHandler;
+using Poco::Net::HTTPRequestHandlerFactory;
+using Poco::Net::HTTPServer;
+using Poco::Net::HTTPServerRequest;
+using Poco::Net::HTTPServerResponse;
+using Poco::Net::HTTPServerParams;
+using Poco::Timestamp;
+using Poco::DateTimeFormatter;
+using Poco::DateTimeFormat;
+using Poco::ThreadPool;
+using Poco::Util::ServerApplication;
+using Poco::Util::Application;
+using Poco::Util::Option;
+using Poco::Util::OptionSet;
+using Poco::Util::HelpFormatter;
 
 class MainServer : public Poco::Util::ServerApplication {
-  public:
-    MainServer() : _helpRequested(false) {
+ public:
+  MainServer() : _helpRequested(false) {
+    addSubsystem(new DataManager);
+    addSubsystem(new ChatHttpSubSystem);
+  }
 
-        //使用环境变量配置数据库
-        string connectionString;
-        if (Poco::Environment::has("DATABASE_CONFIG")) {
-            connectionString = Poco::Environment::get("DATABASE_CONFIG");
-            std::cout<< connectionString << std::endl;
-            Poco::Data::MySQL::Connector::registerConnector();
-        } else {
-            poco_error(logger(), "DATABASE_CONFIG not found");
-            exit(1);
-        }
+  ~MainServer() = default;
 
-        try {
-            addSubsystem(new DataManager(connectionString));
-            addSubsystem(new ChatHttpSubSystem);
-        }
-        catch (Poco::Exception &e) {
-            poco_error(logger(), e.displayText());
-            exit(1);
-        }
-    }
+ protected:
 
-    ~MainServer() = default;
+  void initialize(Application &self) {
+    loadConfiguration(); // load default configuration files, if present
+    ServerApplication::initialize(self);
+    std::string pattern = config().getString("MainServer.logPattern","[%p] - %Y-%m-%d %H:%M:%S (%O:%u) %t");
+    auto* pPF = new Poco::PatternFormatter(pattern);
+    auto* pCC = new Poco::ConsoleChannel;
+    auto* pFC = new Poco::FormattingChannel(pPF,pCC);
 
-  protected:
+    this->logger().setChannel(pFC);
+  }
 
-    void initialize(Application &self) {
-        loadConfiguration(); // load default configuration files, if present
-        ServerApplication::initialize(self);
-        std::string pattern = config().getString("MainServer.logPattern", "[%p] -%J- %Y-%m-%d %H:%M:%S (%O:%u) %t");
-        auto *pPF = new Poco::PatternFormatter(pattern);
-        auto *pCC = new Poco::ConsoleChannel;
-        auto *pFC = new Poco::FormattingChannel(pPF, pCC);
+  void uninitialize() {
+    ServerApplication::uninitialize();
+  }
 
-        this->logger().setChannel(pFC);
-        this->logger().setLevel(config().getString("MainServer.logLevel", "information"));
-    }
+  void defineOptions(OptionSet &options) {
+    ServerApplication::defineOptions(options);
 
-    void uninitialize() {
-        ServerApplication::uninitialize();
-    }
-
-    void defineOptions(OptionSet &options) {
-        ServerApplication::defineOptions(options);
-
-        options.addOption(
-            Option("help", "h", "display help information on command line arguments")
-                .required(false)
-                .repeatable(false));
-        options.addOption(
-            Option("port", "p", "set port number")
-                .required(false)
-                .repeatable(false)
-                .argument("number")
-                .binding("port")
+    options.addOption(
+        Option("help", "h", "display help information on command line arguments")
+            .required(false)
+            .repeatable(false));
+    options.addOption(
+        Option("port", "p", "set port number")
+            .required(false)
+            .repeatable(false)
+            .argument("number")
+            .binding("port")
         );
+  }
+
+  void handleOption(const std::string &name, const std::string &value) {
+    ServerApplication::handleOption(name, value);
+    if (name == "help")
+      _helpRequested = true;
+
+  }
+
+  void displayHelp(){
+    HelpFormatter helpFormatter(options());
+    helpFormatter.setCommand(commandName());
+    helpFormatter.setUsage("OPTIONS");
+    helpFormatter.setHeader("A web server holds a chatroom server");
+    helpFormatter.format(std::cout);
+  }
+
+  int main(const std::vector<std::string> &args) {
+
+    poco_information(logger(),"MainServerStarted");
+    if (_helpRequested) {
+      displayHelp();
+    } else {
+      unsigned short port = (unsigned short) config().getInt("MainServer.port", 12300);
+      std::string format(config().getString("MainServer.format", DateTimeFormat::SORTABLE_FORMAT));
+      ThreadPool::defaultPool();
+      waitForTerminationRequest();
     }
+    return Application::EXIT_OK;
+  }
 
-    void handleOption(const std::string &name, const std::string &value) {
-        ServerApplication::handleOption(name, value);
-        if (name == "help")
-            _helpRequested = true;
-
-    }
-
-    void displayHelp() {
-        HelpFormatter helpFormatter(options());
-        helpFormatter.setCommand(commandName());
-        helpFormatter.setUsage("OPTIONS");
-        helpFormatter.setHeader("A web server holds a chatroom server");
-        helpFormatter.format(std::cout);
-    }
-
-    int main(const std::vector<std::string> &args) {
-
-        poco_information(logger(), "MainServerStarted");
-        if (_helpRequested) {
-            displayHelp();
-        } else {
-            unsigned short port = (unsigned short) config().getInt("MainServer.port", 12300);
-            std::string format(config().getString("MainServer.format", DateTimeFormat::SORTABLE_FORMAT));
-            ThreadPool::defaultPool().addCapacity(config().getInt("MainServer.threadPoolCapacity", 16));
-            waitForTerminationRequest();
-        }
-        return Application::EXIT_OK;
-    }
-
-  private:
-    bool _helpRequested;
+ private:
+  bool _helpRequested;
 };
 
 int main(int argc, char **argv) {
-    MainServer server;
-    return server.run(argc, argv);
+  MainServer server;
+  return server.run(argc, argv);
 }
--- server/net/Route.h
+++ server/net/Route.h
@@ -8,193 +8,146 @@
 #include <functional>
 #include <regex>
 
-#include "poco_headers.h"
-#include "DataManager.h"
-#include "chat_jwt.h"
-#include "state_code.h"
-#include "DataFacade.h"
+#include "Poco/Net/HTTPServer.h"
+#include "Poco/Net/WebSocket.h"
+#include "Poco/Net/ServerSocket.h"
+#include "Poco/Net/HTTPRequestHandlerFactory.h"
+#include "Poco/Net/HTTPRequestHandler.h"
+#include "Poco/Net/HTTPServerRequest.h"
+#include "Poco/Net/HTTPServerResponse.h"
+#include "Poco/Net/WebSocket.h"
+#include "Poco/Net/HTTPServerParams.h"
+#include "Poco/Net/HTTPServerRequestImpl.h"
+#include "Poco/Net/HTTPServerResponseImpl.h"
+#include "Poco/Net/HTTPRequestHandlerFactory.h"
+#include "Poco/Net/HTTPRequestHandler.h"
+#include "Poco/Net/HTTPServerRequest.h"
+#include "Poco/Net/HTTPServerResponse.h"
+#include "Poco/Net/WebSocket.h"
+#include "Poco/Net/HTTPServerParams.h"
+#include "Poco/Util/ServerApplication.h"
+#include "Poco/Util/OptionSet.h"
+#include "Poco/Util/OptionCallback.h"
+#include "Poco/Util/Option.h"
+#include "Poco/Util/HelpFormatter.h"
+#include "Poco/Util/AbstractConfiguration.h"
+#include "Poco/Util/LoggingConfigurator.h"
+#include "Poco/Util/PropertyFileConfiguration.h"
+#include "Poco/Util/Application.h"
+#include "Poco/Net/HTTPRequest.h"
+#include "Poco/Timestamp.h"
+#include "Poco/DateTimeFormatter.h"
+#include "Poco/DateTimeFormat.h"
+#include <Poco/JSON/Object.h>
+#include <Poco/JSON/Parser.h>
+
+using Poco::Net::ServerSocket;
+using Poco::Net::HTTPRequestHandler;
+using Poco::Net::HTTPRequestHandlerFactory;
+using Poco::Net::HTTPServer;
+using Poco::Net::HTTPServerRequest;
+using Poco::Net::HTTPServerResponse;
+using Poco::Net::HTTPServerParams;
+using Poco::Timestamp;
+using Poco::DateTimeFormatter;
+using Poco::DateTimeFormat;
+using Poco::ThreadPool;
+using Poco::Util::ServerApplication;
+using Poco::Util::Application;
+using Poco::Util::Option;
+using Poco::Util::OptionSet;
+using Poco::Util::HelpFormatter;
 
 struct RouteKey {
-    string requestType;
-    std::regex pattern;
+  std::string requestType;
+  std::regex pattern;
 };
 using Route = std::pair<RouteKey, std::function<void(HTTPServerRequest &, HTTPServerResponse &)>>;
-inline DataManager &DM_instance() {
-    return Poco::Util::Application::instance().getSubsystem<DataManager>();
-}
+
 inline std::vector<Route> routeTable{
-    //CORS
-    {
-        {"OPTIONS", std::regex("/.*")},
-        [](HTTPServerRequest &request, HTTPServerResponse &response) {
-            // 设置CORS响应头
-//          poco_debug_f1(Application::instance().logger(), "request.getURI() = %s", request.getURI());
-            response.add("Access-Control-Allow-Origin", "*");  // 这里你也可以设置为你的前端域名，而不是'*'
-            response.add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
-//            response.add("Access-Control-Allow-Headers", "Authorization, Content-Type");
-//            response.set("Access-Control-Expose-Headers", "Authorization");
-
-
-            response.set("Author", "Lambert");
-            response.send();
-        }
-    },
-
     // Register
     {{"POST", std::regex("/register")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-         poco_debug_f1(Application::instance().logger(), "request.getURI() = %s", request.getURI());
-         response.add("Access-Control-Allow-Origin", "*");
-         response.add("Access-Control-Allow-Headers", "Authorization");
+       response.setChunkedTransferEncoding(true);
+       response.setContentType("application/json");
+       std::ostream &ostr = response.send();
+       Poco::JSON::Object::Ptr json = new Poco::JSON::Object;
+       Poco::JSON::Object::Ptr dataObject = new Poco::JSON::Object;
+       dataObject->set("id", 0);
+       json->set("code", 0);
+       json->set("msg", "ok");
+       json->set("data", dataObject);
 
-         response.setContentType("application/json");
-
-         std::ostream &ostr = response.send();
-
-         Poco::JSON::Parser parser;
-         auto res = parser.parse(request.stream()).extract<Poco::JSON::Object::Ptr>();
-
-         // 处理JSON数据...
-         auto username = res->getValue<string>("username");
-         auto password = res->getValue<string>("password");
-         auto email = res->getValue<string>("email");
-
-         if (username.empty() || password.empty()) {
-             response.setStatusAndReason(Poco::Net::HTTPResponse::HTTP_BAD_REQUEST);
-             return;
-         }
-
-         Poco::JSON::Object::Ptr json = DataFacade::registerUser(username, password, email);
-         json->stringify(ostr);
+       json->stringify(ostr);
      }},
-
-
     // Login
     {{"POST", std::regex("/login")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-         // handle login request
-         response.setContentType("application/json");
-         response.add("Access-Control-Allow-Origin", "*");
-         response.add("Access-Control-Allow-Methods", "POST");
-//         response.add("Access-Control-Allow-Headers", "Authorization, Content-Type");
-//         response.set("Access-Control-Expose-Headers", "Authorization");
-
-         try {
-             string jwt = request.get("Authorization", "");
-             if (!jwt.empty())
-                 jwt = jwt.substr(7);
-             Poco::JWT::Signer signer(CONST_CONFIG::kTokenSecret);
-             Poco::JWT::Token token = signer.verify(jwt);
-             //We only process situation that token is valid
-             if (token.getExpiration() > Poco::Timestamp()) {
-                 Poco::JSON::Object::Ptr json = new Poco::JSON::Object();
-                 json->set("code", 0);
-                 json->set("msg", "JWT验证成功");
-                 Poco::JSON::Object::Ptr data = new Poco::JSON::Object();
-                 data->set("username", token.payload().get("username"));
-                 data->set("id", token.payload().get("id"));
-                 data->set("email", token.payload().get("email"));
-                 json->set("data", data);
-                 json->stringify(response.send());
-             }
-
-         }
-         catch (Poco::Exception &e) {
-
-             Poco::JSON::Parser parser;
-             auto res = parser.parse(request.stream()).extract<Poco::JSON::Object::Ptr>();
-             auto username = res->getValue<string>("username");
-             auto password = res->getValue<string>("password");
-             auto id = res->getValue<int>("id");
-
-             poco_information_f1(Application::instance().logger(), "Exception: %s", e.displayText());
-             Poco::JSON::Object::Ptr json = DataFacade::loginUser(username, id, password);
-             if (json->getValue<int>("code") == 0) {
-                 response.set("Authorization", "Bearer " + setJWT(id, username));
-             }
-             json->stringify(response.send());
-         }
-
+       // handle login request
      }},
     // Get Chatrooms List
     {{"GET", std::regex("/chatrooms")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-
-         int id;
-         try {
-             auto jwt = request.get("Authorization", "");
-             Poco::JWT::Signer signer{CONST_CONFIG::kTokenSecret};
-             Poco::JWT::Token token = signer.verify(jwt);
-             id = token.payload().get("user_id");
-         }
-         catch (Poco::Exception &e) {
-             Poco::JSON::Object::Ptr json = new Poco::JSON::Object();
-             json->set("code", static_cast<int>(state_code::VERIFY_TOKEN_FAILED));
-             json->set("msg", "JWT验证失败");
-             json->stringify(response.send());
-         }
-
-         Poco::JSON::Object::Ptr result = DataFacade::getRoomList(id);
-            result->stringify(response.send());
+       // handle get chatrooms list request
      }},
     // Create Chatroom
     {{"POST", std::regex("/chatroom")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-         // handle create chatroom request
+       // handle create chatroom request
      }},
     // Get Chatroom Info
     {{"GET", std::regex("/chatrooms/.+")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-         // handle get chatroom info request
+       // handle get chatroom info request
      }},
     // Get Chatroom Member Info
     {{"GET", std::regex("/chatrooms/.+/member")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-         // handle get chatroom member info request
+       // handle get chatroom member info request
      }},
     // Update Chatroom Info
     {{"PUT", std::regex("/chatrooms/.+")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-         // handle update chatroom info request
+       // handle update chatroom info request
      }},
     // Delete Chatroom
     {{"DELETE", std::regex("/chatrooms/.+")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-         // handle delete chatroom request
+       // handle delete chatroom request
      }},
     // Add Chatroom Member
     {{"POST", std::regex("/chatrooms/.+/member")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-         // handle add chatroom member request
+       // handle add chatroom member request
      }},
     // Delete Chatroom Member
     {{"DELETE", std::regex("/chatrooms/.+/member")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-         // handle delete chatroom member request
+       // handle delete chatroom member request
      }},
     // Get Users List
     {{"GET", std::regex("/users")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-         // handle get users list request
+       // handle get users list request
      }},
     // Get User Info
     {{"GET", std::regex("/users/.+")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-         // handle get user info request
+       // handle get user info request
      }},
     // Update User Info
     {{"PUT", std::regex("/users/.+")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-         // handle update user info request
+       // handle update user info request
      }},
     // Delete User
     {{"DELETE", std::regex("/users/.+")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-         // handle delete user request
+       // handle delete user request
      }},
     // Index Entrance
     {{"GET", std::regex("/")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-         // handle index entrance request
+       // handle index entrance request
      }}
 };
