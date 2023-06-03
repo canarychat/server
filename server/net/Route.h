4870f911fed1c57fff4a120df1dc60f75f33ce55
Lambert-Rao <lambert-teddy-smith@outlook.com>
2023-06-03 10:24:23 +0800

    feat: add login

--- server/net/Route.h
+++ server/net/Route.h
@@ -12,126 +12,141 @@
 #include "DataManager.h"
 
 struct RouteKey {
-  std::string requestType;
-  std::regex pattern;
+    string requestType;
+    std::regex pattern;
 };
 using Route = std::pair<RouteKey, std::function<void(HTTPServerRequest &, HTTPServerResponse &)>>;
 inline DataManager &DM_instance() {
-  return Poco::Util::Application::instance().getSubsystem<DataManager>();
+    return Poco::Util::Application::instance().getSubsystem<DataManager>();
 }
 inline std::vector<Route> routeTable{
     //CORS
     {
         {"OPTIONS", std::regex("/.*")},
         [](HTTPServerRequest &request, HTTPServerResponse &response) {
-          // 设置CORS响应头
+            // 设置CORS响应头
 //          poco_debug_f1(Application::instance().logger(), "request.getURI() = %s", request.getURI());
-          response.add("Access-Control-Allow-Origin", "*");  // 这里你也可以设置为你的前端域名，而不是'*'
-          response.add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
-          response.add("Access-Control-Allow-Headers", "Content-Type");
-          response.send();
+            response.add("Access-Control-Allow-Origin", "*");  // 这里你也可以设置为你的前端域名，而不是'*'
+            response.add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+            response.add("Access-Control-Allow-Headers", "Content-Type");
+            response.send();
         }
     },
 
     // Register
     {{"POST", std::regex("/register")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-       poco_debug_f1(Application::instance().logger(), "request.getURI() = %s", request.getURI());
-       response.add("Access-Control-Allow-Origin", "*");
-       response.setContentType("application/json");
+         poco_debug_f1(Application::instance().logger(), "request.getURI() = %s", request.getURI());
+         response.add("Access-Control-Allow-Origin", "*");
+         response.setContentType("application/json");
 
-       std::ostream &ostr = response.send();
+         std::ostream &ostr = response.send();
 
-       Poco::JSON::Parser parser;
-       auto res = parser.parse(request.stream()).extract<Poco::JSON::Object::Ptr>();
+         Poco::JSON::Parser parser;
+         auto res = parser.parse(request.stream()).extract<Poco::JSON::Object::Ptr>();
 
-       // 处理JSON数据...
-       auto username = res->getValue<std::string>("username");
-       auto password = res->getValue<std::string>("password");
-       auto email = res->getValue<std::string>("email");
+         // 处理JSON数据...
+         auto username = res->getValue<string>("username");
+         auto password = res->getValue<string>("password");
+         auto email = res->getValue<string>("email");
 
-       if (username.empty() || password.empty()) {
-         response.setStatusAndReason(Poco::Net::HTTPResponse::HTTP_BAD_REQUEST);
-         return;
-       }
+         if (username.empty() || password.empty()) {
+             response.setStatusAndReason(Poco::Net::HTTPResponse::HTTP_BAD_REQUEST);
+             return;
+         }
 
-       auto json = DM_instance().registerUser( username, password, email);
-           json->stringify(ostr);
+         auto json = DM_instance().registerUser(username, password, email);
+         json->stringify(ostr);
      }},
 
 
     // Login
     {{"POST", std::regex("/login")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-       // handle login request
-       response.add("Access-Control-Allow-Origin", "*");
-       response.setContentType("application/json");
-       std::ostream &ostr = response.send();
+         // handle login request
+         response.add("Access-Control-Allow-Origin", "*");
+         response.setContentType("application/json");
+         std::ostream &ostr = response.send();
 
+         Poco::JSON::Parser parser;
+         auto res = parser.parse(request.stream()).extract<Poco::JSON::Object::Ptr>();
+
+         // 处理JSON数据...
+         auto username = res->getValue<string>("username");
+         auto password = res->getValue<string>("password");
+         auto id = res->getValue<int>("id");
+
+         if ((username.empty() && id == 0) || password.empty()) {
+             response.setStatusAndReason(Poco::Net::HTTPResponse::HTTP_BAD_REQUEST);
+             return;
+         }
+
+         auto json = DM_instance().loginUser(username,  id, password);
+         json->stringify(ostr);
      }},
     // Get Chatrooms List
     {{"GET", std::regex("/chatrooms")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-       // handle get chatrooms list request
+         // handle get chatrooms list request
      }},
     // Create Chatroom
     {{"POST", std::regex("/chatroom")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-       // handle create chatroom request
+         // handle create chatroom request
      }},
     // Get Chatroom Info
     {{"GET", std::regex("/chatrooms/.+")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-       // handle get chatroom info request
+         // handle get chatroom info request
      }},
     // Get Chatroom Member Info
     {{"GET", std::regex("/chatrooms/.+/member")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-       // handle get chatroom member info request
+         // handle get chatroom member info request
      }},
     // Update Chatroom Info
     {{"PUT", std::regex("/chatrooms/.+")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-       // handle update chatroom info request
+         // handle update chatroom info request
      }},
     // Delete Chatroom
     {{"DELETE", std::regex("/chatrooms/.+")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-       // handle delete chatroom request
+         // handle delete chatroom request
      }},
     // Add Chatroom Member
     {{"POST", std::regex("/chatrooms/.+/member")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-       // handle add chatroom member request
+         // handle add chatroom member request
      }},
     // Delete Chatroom Member
     {{"DELETE", std::regex("/chatrooms/.+/member")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-       // handle delete chatroom member request
+         // handle delete chatroom member request
      }},
     // Get Users List
     {{"GET", std::regex("/users")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-       // handle get users list request
+         // handle get users list request
      }},
     // Get User Info
     {{"GET", std::regex("/users/.+")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-       // handle get user info request
+         // handle get user info request
      }},
     // Update User Info
     {{"PUT", std::regex("/users/.+")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-       // handle update user info request
+         // handle update user info request
      }},
     // Delete User
     {{"DELETE", std::regex("/users/.+")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-       // handle delete user request
+         // handle delete user request
      }},
     // Index Entrance
     {{"GET", std::regex("/")},
      [](HTTPServerRequest &request, HTTPServerResponse &response) {
-       // handle index entrance request
+         // handle index entrance request
      }}
 };
